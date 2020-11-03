031ebaa02a80afd95bc375da9d4671924caa6815
Luiz Verissimo <luizverissimosouza@gmail.com>
2020-11-03 08:20:37 -0300

    finish notification

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,45 @@
+const fetch = require("node-fetch");
+const moment = require("moment");
 const functions = require("firebase-functions");
 const connection = require("./database/connection");
 const admin = require("firebase-admin");
 admin.initializeApp();
-const fetch = require("node-fetch");
 
 exports.sendNotifications = functions.https.onRequest(async (req, res) => {
   try {
+    const now = moment(Date.now()).format("YYYY-MM-DDT23:59:00-03");
     const notifications = await connection("notifications")
       .select("*")
+      .join("users", "users.i_user", "=", "notifications.i_user")
+      .where({ "notifications.deleted": false, sent: false })
+      .where("notifications.sent_date", "<=", now);
 
-      .where({ deleted: false, sent: false })
-      .where("visits.aud_created_date", ">=", startDate);
+    await notifications.forEach(async (notification) => {
+      const message = {
+        to: notification.expo_push_token,
+        sound: "default",
+        title: notification.title,
+        body: notification.description,
+        data: {},
+      };
 
-    return res.json(customers);
+      await fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Accept-encoding": "gzip, deflate",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(message),
+      });
 
-    // const message = {
-    //   to: "ExponentPushToken[h3ltswFkSTp0a_-iXxn3-m]",
-    //   sound: "default",
-    //   title: "Original Title",
-    //   body: "And here is the body!",
-    //   data: { data: "goes here" },
-    // };
-
-    // await fetch("https://exp.host/--/api/v2/push/send", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Accept-encoding": "gzip, deflate",
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(message),
-    // });
+      await connection("notifications")
+        .where({ i_notification: notification.i_notification })
+        .update({ sent: true });
+    });
     return res.json({ ok: "ok" });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ error: error });
   }
 });
